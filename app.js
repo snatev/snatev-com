abd89ce716fb3b386f2e06efceed2a86dfd9fe05
snatev <snatev@proton.me>
2025-03-27 23:01:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const rateLimit = require('express-rate-limit');
 
 const app = express();
 app.use(compression());
-
+app.set('trust proxy', true);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
