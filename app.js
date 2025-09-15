ffffe6f6f66ff89552bfcbf524691fbda33a7fa3
snatev <mail@snatev.com>
2025-09-15 19:10:37 +0200

    Structure changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const helmet = require('helmet');
 const express = require('express');
 const compression = require('compression');
 const rateLimit = require('express-rate-limit');
@@ -10,29 +9,6 @@ app.use(compression());
 app.set('trust proxy', 1);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-
-app.use(helmet({
-    hsts: {
-        preload: true,
-        maxAge: 31536000,
-        includeSubDomains: true
-    },
-    contentSecurityPolicy: {
-        directives: {
-            mediaSrc: ["'self'"], objectSrc: ["'none'"],
-            imgSrc: ["'self'", "data:", "https://snatev.com"],
-            defaultSrc: ["'self'"], frameSrc: ["'none'"], connectSrc: ["'self'"],
-            scriptSrc: [
-                "'self'",
-                "'unsafe-inline'",
-                "https://cdnjs.cloudflare.com",
-                "https://static.cloudflareinsights.com"
-            ],
-            fontSrc: ["'self'", "https://cdnjs.cloudflare.com", "https://fonts.gstatic.com"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com", "https://fonts.googleapis.com"]
-        }
-    }
-}));
 
 const limiter = rateLimit({
     max: 1000,
